4eb82f79f726b414bce2a1871d99a716a6b29556
Felipe Marinho <felipevm97@gmail.com>
2021-11-08 10:42:21 -0300

    new: feat: add REST gateway implementation

---
+++ rest/go.mod
@@ -0,0 +1,19 @@
+module github.com/felipemarinho97/e-commerce/rest
+
+go 1.17
+
+require (
+	github.com/golang/protobuf v1.5.2
+	github.com/gosidekick/goconfig v1.3.1
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0
+	google.golang.org/genproto v0.0.0-20211104193956-4c6863e31247
+	google.golang.org/grpc v1.42.0
+	google.golang.org/protobuf v1.27.1
+)
+
+require (
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/text v0.3.5 // indirect
+)
---
+++ rest/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"google.golang.org/grpc"
+
+	pb "github.com/felipemarinho97/e-commerce/rest/api" // Update
+	"github.com/felipemarinho97/e-commerce/rest/config"
+)
+
+func run() error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	cfg := config.Get()
+
+	// Register gRPC server endpoint
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err := pb.RegisterEcommerceServiceHandlerFromEndpoint(ctx, mux, fmt.Sprintf("%s:%d", cfg.Host, cfg.GRPCPort), opts)
+	if err != nil {
+		return err
+	}
+
+	// Start HTTP server (and proxy calls to gRPC server endpoint)
+	return http.ListenAndServe(fmt.Sprintf("%s:%d", cfg.Host, cfg.Port), mux)
+}
+
+func main() {
+	cfg := config.Get()
+
+	log.Printf("server listening at %v", fmt.Sprintf("%s:%d", cfg.Host, cfg.Port))
+	if err := run(); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
